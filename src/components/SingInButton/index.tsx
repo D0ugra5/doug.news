84b5aad9cc7886e8c877b482992984a9bd457137
DevsJabinho <douglasmb87@gmail.com>
2021-04-08 00:36:47 -0300

    'att08-04-2021'

---
+++ src/components/SingInButton/index.tsx
@@ -0,0 +1,28 @@
+import {FaGithub} from 'react-icons/fa'
+import {FiX} from 'react-icons/fi'
+import styles from './styles.module.scss'
+export function SingInButton(){
+    const isUserLoggedIn = false 
+    return isUserLoggedIn ? (
+        <button
+        type="button"
+        className={styles.signinButton}
+        >
+            <FaGithub color="#84d361"/>
+           Douglinhas Douglas
+           <FiX color="#737380" className={styles.closedIcon} />
+        </button>
+  ):(
+    <button
+        type="button"
+        className={styles.signinButton}
+        >
+            <FaGithub color="#eba417"/>
+            Sing on with Github
+        </button>
+  )
+
+
+    
+    
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import style from   '../styles/home.module.scss'
+
 export default function Home() {
   return (
-<h1 className={style.title}>Hello World</h1>
+<h1></h1>
   )
 }

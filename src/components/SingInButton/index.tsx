8da61b4f5c10853efe21daa0276d832e6a9315a1
Douglas <douglasmb87@gmail.com>
2021-04-08 13:09:50 -0300

    '08/04/2021'

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -2,7 +2,7 @@ import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
 import styles from './styles.module.scss'
 export function SingInButton(){
-    const isUserLoggedIn = false 
+    const isUserLoggedIn = true 
     return isUserLoggedIn ? (
         <button
         type="button"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
-
+import  Head  from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from './home.module.scss'
 export default function Home() {
   return (
-<h1></h1>
+    <> 
+<Head>
+  <title>Home| Doug.News</title>
+</Head>
+<main className={styles.contentContainer}>
+  <section className={styles.hero}>
+    <span>👏 Hey, welcome</span>
+    <h1>News about the <span>World.</span></h1>
+    <p>
+      Get Acess to all the Publications <br/>
+      <span>For $9.90 month</span>
+    </p>
+    <SubscribeButton/>
+  </section>
+  <img src="/images/avatar.svg" alt="girl coding "/>
+</main>
+</>
   )
 }
